31e8e6d247756759494b942e27e2a38d41bd9a55
Mac <mac@Macs-iMac.local>
2017-08-08 00:26:46 +0530

    Calculator first commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,93 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var displaylbl: UILabel!
+    
+    var displayValue : Double{
+        get{
+        return Double(displaylbl.text!)!
+        }
+        set{
+        displaylbl.text = String(newValue)
+        }
+    }
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func TouchDigitAction(_ sender: UIButton) {
+    
+        let digit = sender.currentTitle
+        
+        if userIsInTheMiddleOfTyping{
+        
+            let currentTextInDisplay = displaylbl.text!
+            
+            displaylbl.text = currentTextInDisplay + digit!
+        }
+        else{
+           displaylbl.text = digit
+            userIsInTheMiddleOfTyping = true
+        
+        }
+    
+}
+    
+    
+    @IBAction func DeleteActionButton(_ sender: UIButton) {
+        
+        if userIsInTheMiddleOfTyping {
+            
+            var numChars = displaylbl.text?.characters.count ?? 0 // 0
+           
+            numChars = displaylbl.text?.characters.count ?? 0
+            
+            if numChars > 0 {
+                
+                var name: String = displaylbl.text!
+                name.remove(at: name.index(before: name.endIndex))
+                print(name)
+                
+                displaylbl.text = name
+                
+            }
+            else{
+            
+            displaylbl.text = ""
+            
+            }
+        }
+        
     }
-
-
+    private var brain = CalculatorBrain()
+    
+         @IBAction func PerformOperationAction(_ sender: UIButton) {
+    
+        if userIsInTheMiddleOfTyping {
+            
+        brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        
+        }
+        if  let mathematicalsymbol = sender.currentTitle {
+             brain.performOperation(mathematicalsymbol)
+        }
+        if  let result = brain.result {
+            displayValue = result
+        }
+    
+    }
+    
 }
 
